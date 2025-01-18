12ae204f02f0e4db6d6dcc955a7c75163b200149
NIkunj Miglani <niikkunjmiglani@gmail.com>
2025-01-18 12:20:23 +0530

    Update page.js

--- page.js
+++ page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,18 +14,18 @@ export default function Home() {
 
         <div>
           <div className="py-4 flex gap-2">
-            <button
+           <Link href="/knowmore"> <button 
               type="button"
               className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             >
-              Start Now
-            </button>
-            <button
+              Know More
+            </button></Link>
+            <Link href="/login"><button
               type="button"
               className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             >
               Sign Up
-            </button>
+            </button></Link>
           </div>
         </div>
       </div>
