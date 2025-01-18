3648cc8ac2d165ad683942068af98d8754fa97c3
NIkunj Miglani <niikkunjmiglani@gmail.com>
2025-01-18 12:20:55 +0530

    Add files via upload

---
+++ knowmore/page.js
@@ -0,0 +1,27 @@
+"use client"
+import React from 'react'
+
+
+const Knowmore = () => {
+  return (
+    <div>
+      <h1 className="text-center py-5 px-5 font-bold text-3xl">About Us</h1>
+        <p className='text-xl px-4 text-pretty m-5  mx-auto font-medium'>
+            A crowdfunding platform for creators. Get funded by your fans and followers. Start Now! 
+            Welcome to Get Me A Chai  the ultimate crowdfunding platform where creativity, passion, and community come together. Inspired by the spirit of generosity and support, we aim to empower creators, dreamers, and innovators by providing a space where they can turn their visions into reality.
+
+At Get Me A Chai, we believe in the power of small contributions making a big difference. Whether youre an artist, writer, musician, podcaster, or educator, our platform allows you to connect directly with your audience and receive support for your work  one cup of chai at a time.
+
+Our mission is simple: to create a welcoming space where creators can share their stories, build meaningful relationships with their supporters, and sustain their creative journeys. We are committed to transparency, fairness, and ensuring every creator has the tools they need to thrive.
+
+What sets us apart? We focus on simplicity, inclusivity, and community-driven support. With easy-to-use tools, customizable pages, and flexible payment options, Get Me A Chai makes crowdfunding accessible to everyone.
+
+Join us in fueling creativity and making dreams come true. Whether youre here to support your favorite creators or start your own campaign, together we can make a positive impact one chai at a time.
+
+Start your journey with Get Me A Chai today and become part of a community where creativity is celebrated and supported.
+        </p>
+    </div>
+  )
+}
+
+export default Knowmore
